084f72fb9e2fb06267f4270f179e7f69ca6117a9
Jonathan <jm-191091@hs-weingarten.de>
2021-04-21 14:05:09 +0200

    k

--- server.js
+++ server.js
@@ -1,109 +1,106 @@
+const PythonShell = require('python-shell');
+const { exec } = require("child_process");
 const express = require('express');
 const cors = require("cors");
-const app = express();
 const fs = require("fs");
-const { exec } = require("child_process");
-const PORT = 33333
 const filename = __dirname + "/test.json";
-
+const PORT = 33333
+const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 // Middleware
+var python_process;
+var running = false;
+
 function log(req, res, next) {
   console.log(req.method + " Request at " + req.url);
   next();
 }
 app.use(log);
 
-function off(){
-  var killcmd ='./led_scripts/kill.sh && python led_scripts/off.py'
-  //var cmd = "python led_scripts/off.py"
-  exec(killcmd, (error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-      }
-      //stdout:
-      console.log(`${stdout}`);
-  });
+function lights_off(){
+    if (running){
+        python_process.kill('SIGINT');
+        running = false;
+    }
+    var cmd = "python led_scripts/off.py"
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        //stdout:
+        console.log(`${stdout}`);
+    });
 }
 
-//Endpunkte
+//Endpoints
+app.get('/off', function(req,res) {
+    lights_off();
+    res.writeHead(200, {
+        "Content-Type": "application/json",
+    });
+    res.end("success");
+});
+
+app.get('/rain', function(req,res) {
+    lights_off();
+    var pyshell = new PythonShell('led_scripts/rain.py');
+    pyshell.end(function(err){
+    if(err){
+        console.log(err);}
+    });
+    python_process = pyshell.childProcess;
+
+    res.writeHead(200, {
+        "Content-Type": "application/json",
+    });
+    res.end("success");
+});
+
+app.get('/dimm', function(req,res) {
+    lights_off();
+    var cmd = "python led_scripts/dimm.py"
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+             console.log(`stderr: ${stderr}`);
+            return;
+        }
+        //stdout:
+        console.log(`${stdout}`);
+    });
+    res.writeHead(200, {
+        "Content-Type": "application/json",
+    });
+    res.end("success");
+});
+
+app.get("/test", function (req, res) {
+    fs.readFile(filename, "utf8", function (err, data) {
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+        });
+        res.end(data);
+    });
+});
+
 // ignore this
 app.get('/favicon.ico', function(req, res) {
     res.status(204);
     res.end();
 });
 
-// command
-app.get('/dimm', function(req,res) {
-  var cmd = "python led_scripts/dimm.py"
-  exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-      }
-      //stdout:
-      console.log(`${stdout}`);
-  });
-  res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-    res.end("dimm");
-});
-
-// command
-app.get('/rain', function(req,res) {
-  off();
-  var cmd = "python led_scripts/rain.py"
-  exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-      }
-      //stdout:
-      console.log(`${stdout}`);
-  });
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-  });
-  res.end("rain");
-});
-
-// command
-app.get('/off', function(req,res) {
-  off();
-  res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-    res.end("off");
-});
-
-// test
-app.get("/test", function (req, res) {
-  fs.readFile(filename, "utf8", function (err, data) {
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-    res.end(data);
-  });
-});
-
-
 // Start server
 app.listen(PORT, () => {
-  console.log('Server listening for requests on port ' + PORT);
+    console.log('Server listening for requests on port ' + PORT);
 });
