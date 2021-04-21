b456ab9d9f00fa4cdf81a88bf5048eb6abc85eb8
Jonathan <jm-191091@hs-weingarten.de>
2021-04-21 17:26:14 +0200

    k

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-btn outlined color="success">
+          Off
+     </v-btn>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
