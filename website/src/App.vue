eda571a2a6be5c048cd45e10c4b220f95f23cd3f
Jonathan <jm-191091@hs-weingarten.de>
2021-04-21 17:35:17 +0200

    k

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <v-btn outlined color="success">
+    <v-btn @click="offclicked" outlined color="success">
           Off
      </v-btn>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    
+  name: 'Default-site',
+  data(){
+    return {
+        backendURL: '192.168.0.11:33333',
+
+    },
+     mounted() {
+        // called when site is first visited/refreshed
+
+     },
+     methods: {
+        offclicked: function() {
+            axios.get(this.backendURL + 'off/').then(response => {
+               console.log(response.data);
+            });
+        }
+     }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
