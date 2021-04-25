1caa45f801fcd8182f08bfafe6161b4597da1d53
Benedikt <s17dcc@umwelt-campus.de>
2021-04-25 20:43:56 +0100

    reworked start and stop script

--- start.sh
+++ start.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
-pidfile_led=/tmp/led.pid
-pidfile_node=/tmp/node.pid
+# holds the Process IDs 
+PIDFILE=/tmp/LED.pid
 
+
+# taken from pihole installer script
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+NC='\e[0m'
+TICK="[${GREEN}✓${NC}]"
+CROSS="[${RED}✗${NC}]"
+
+
+# create folder and logfiles if not present
 file=$(realpath $0)
 root=$(dirname $file)
-
-
 mkdir -p $root/log/
-> $root/log/led.log
-> $root/log/node.log
+>> $root/log/led.log
+>> $root/log/node.log
 
-# see if led PID exists
-if [ -e "$pidfile_led" ]; then
-    echo "warning: led server already running or previous dirty shutdown"
-    echo "remove file '$pidfile_led' and try again"
+
+# see if PID exists
+if [ -e "$PIDFILE" ]; then
+    echo -e "$CROSS Server already running or previous dirty shutdown"
+    echo "if server not running, remove file '$PIDFILE' and try again"
     exit 1
 fi
 
-#see if node PID exists
-if [ -e "$pidfile_node" ]; then
-    echo "warning: node server already running or previous dirty shutdown"
-    echo "remove file '$pidfile_node' and try again"
-    exit 1
-fi
 
 # start led server
+echo -n "[i] Starting led server..."
 nohup python3 $root/led_scripts/led.py &> /dev/null &
 PID_LED=$! 
-echo $PID_LED > $pidfile_led
-echo "led server started:  $PID_LED"
+echo $PID_LED >> $PIDFILE
+sleep 1
+echo -e "\r$TICK $PID_LED led server running"
+
 
 #s start node server
+echo -n "[i] Starting node server..."
 nohup node $root/server.js &>> $root/log/node.log &
 PID_NODE=$!
-echo $PID_NODE > $pidfile_node
-echo "node server started: $PID_NODE"
+echo $PID_NODE >> $PIDFILE
+sleep 1
+echo -e "\r$TICK $PID_NODE node server running"
--- stop.sh
+++ stop.sh
@@ -1,53 +1,44 @@
 #!/bin/bash
 
-pidfile_led=/tmp/led.pid
-pidfile_node=/tmp/node.pid
+# holds the Process IDs 
+PIDFILE=/tmp/LED.pid
+
+
+# taken from pihole installer script
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+NC='\e[0m'
+TICK="[${GREEN}✓${NC}]"
+CROSS="[${RED}✗${NC}]"
+
 
 # see if PID files exist
-if [ ! -e "$pidfile_led" ]; then
-    echo "led server not running"
+if [ ! -e "$PIDFILE" ]; then
+    echo "server not running"
     exit 0
 fi
 
-if [ ! -e "$pidfile_node" ]; then
-    echo "node server not running"
-    exit 0
-fi
 
-PID_LED=$(<$pidfile_led)
-PID_NODE=$(<$pidfile_node)
-
-#see if led process runs
-if ps -p $PID_LED > /dev/null  
-then
-    kill -s SIGINT $PID_LED
-    if ! ps -p $PID_LED > /dev/null
+while read PID
+do
+    # see if process is running
+    name=$(ps -q $PID -o comm=)
+    if [ -n "$name" ]
     then
-        echo "led server  $PID_LED stopped"
+        kill -s SIGINT $PID
+        # see if process has been killed
+        sleep 0.05
+        process=$(ps -q $PID -o comm=)
+        if [ -z "$process" ]
+        then
+            echo -e "$TICK stopped process $name $PID"
+        else
+            echo -e "$CROSS could not stop process $name $PID"
+        fi
     else
-        echo "could not stop led server $PID_LED"
+        echo -e "$CROSS process $PID not running"
     fi
-else
-    echo "led server $PID_LED not running"
-fi
+done < $PIDFILE
 
-# see if node process runs
-if ps -p $PID_NODE > /dev/null
-then
-    kill  $PID_NODE
-    if ! ps -p $PID_NODE > /dev/null
-    then
-        echo "node server $PID_NODE stopped"
-    else
-        echo "could not stop node server $PID_NODE"
-    fi
-else
-    echo "node server $PID_NODE not running"
-fi
 
-rm $pidfile_led
-rm $pidfile_node
-
-sleep 3
-reset
-
+rm $PIDFILE
