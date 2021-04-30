dd2bc54af274579e28b4f4a3d62126bb08d3ee0e
joni <jugglingballz94@gmail.com>
2021-04-30 10:29:27 +0100

    current state

--- res/app.js
+++ res/app.js
@@ -1,21 +1,25 @@
-// OFF / DIMM BUTTONS
-let simple_buttons = Array();
-simple_buttons.push(document.getElementById("off"));
-simple_buttons.push(document.getElementById("dimm"));
-// RAINBOW button
-let rainbow = document.getElementById("rainbow");
+// -- Dom Objects --
 
-// speed slider
+// OFF / DIMM BUTTONS
+let off_button = document.getElementById("off");
+let dimm_button = document.getElementById("dimm");
+// RAINBOW BUTTON
+let rainbow = document.getElementById("rainbow");
+// SPEED SLIDER
 let slider_container = document.getElementById("slider-container");
 let slider = document.getElementById("slider");
 let output = document.getElementById("slider-output");
-
+// COLOR INPUT
+let color = document.getElementById("colorwheel");
+// CONNECTION BOX
+let connection_status = document.getElementById("connection-status");
 // hide slider by default and set default value
 slider_container.style.display = "none";
 output.innerHTML = slider.value;
 
-// colorpicker
-let color = document.getElementById("colorwheel");
+
+//  -- Helper Functions --
+//#region 
 
 // sends a GET request to server
 function sendRequest(destination, args="") {
@@ -24,46 +28,72 @@ function sendRequest(destination, args="") {
     if(args != "") {
         url += "/" + args;
     }
-    console.log(url);
+    //console.log(url);
     request.open("GET", url);
     request.send();
 }
 
-// OFF / DIMM BUTTONS
-simple_buttons.forEach(button => {
-    button.addEventListener("click", event => {
-        // disable rainbow speed slider when visible
-        if(slider_container.style.display === "block") {
-            slider_container.style.display = "none";
-        }
-        // GET request to server
-        const destination = event.target.id;  // off | dimm
-        sendRequest(destination);
-    });
+// calculate speed
+function getSpeed() {
+    // make value small quickly
+    let speed = 1 / (slider.value ** 2);
+    // round
+    speed = Math.ceil(speed * 100000) / 100000;
+    return speed;
+}
+
+//#endregion
+
+
+//  -- Event Handlers for all the buttons --
+//#region 
+
+// OFF BUTTON
+off_button.addEventListener("click", () => {
+    // disable rainbow speed slider when visible
+    if(slider_container.style.display === "block") {
+        slider_container.style.display = "none";
+    }
+    // GET request to server
+    const destination = "off";
+    sendRequest(destination);
 });
 
-// update slider value and send new GET request
-slider.addEventListener("input", event => {
-    const value = event.target.value;
-    output.innerHTML = value;
-    const speed = 1/(value**2);
-    const destination = "rainbow";
-    sendRequest(destination, speed)
+// DIMM BUTTON
+dimm_button.addEventListener("click", () => {
+    // disable rainbow speed slider when visible
+    if(slider_container.style.display === "block") {
+        slider_container.style.display = "none";
+    }
+    // GET request to server
+    const destination = "dimm";
+    sendRequest(destination);
 });
 
-// RAINBOW button
+// RAINBOW BUTTON
 rainbow.addEventListener("click", event => {
     // show speed slider
     if(slider_container.style.display === "none") {
         slider_container.style.display = "block";
     }
     // GET request to server
-    const speed = 1 / (slider.value ** 2);
     const destination = "rainbow";
+    const speed = getSpeed();
+    sendRequest(destination, speed);
+});
+
+// SPEED SLIDER
+slider.addEventListener("input", event => {
+    // display slider value
+    const value = event.target.value;
+    output.innerHTML = value;
+    // GET request to server
+    const destination = "rainbow";
+    const speed = getSpeed();
     sendRequest(destination, speed)
 });
 
-// colorpicker send new request on input
+// COLOR PICKER
 color.addEventListener("input", event => {
     // disable rainbow speed slider when visible
     if(slider_container.style.display === "block") {
@@ -71,6 +101,9 @@ color.addEventListener("input", event => {
     }
     // GET request to server
     const destination = "/colorwheel";
-    const value = event.target.value.slice(1);
-    sendRequest(destination, value);
+    // remove leading '#'
+    const color = event.target.value.slice(1);
+    sendRequest(destination, color);
 });
+
+//#endregion
--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const net = require('net');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(__dirname + "/res"));
 
-const PORT = 33333
+
+const HTTP_PORT = 33333
+const LED_SERVER_PORT = 55555;
 
 function log(req, res, next) {
   console.log(req.method + ' Request at ' + req.url);
   next();
 }
 app.use(log);
+
+//  -- Helper Functions --
+//#region 
 
 // convert msg to JSON and send to server
 function sendMsg(msg, args = '') {
@@ -23,31 +29,49 @@ function sendMsg(msg, args = '') {
   head = data.length.toString().padEnd(5, ' ');
   client.write(head + data, 'utf8');
 }
+//#endregion
 
 // socket setup
+//#region
+let CONNECTION_STATUS = false;
 console.log("creating socket")
 let client = new net.Socket();
 
 // connect handler
 client.on("connect", () => {
-  console.log("connected to server");
+  CONNECTION_STATUS = true;
+  console.log("connected to LED control server");
 });
 
 // error handler
 client.on("error", () => {
+  CONNECTION_STATUS = false;
   console.log("retrying");
   setTimeout(() => {
-    client.connect(55555);
+    client.connect({port: LED_SERVER_PORT});
   }, 10000);
 });
 
+// connect to LED control server
+console.log("connecting to LED control server");
+client.connect({port: LED_SERVER_PORT});
+//#endregion
 
-console.log("connecting to server");
-client.connect({port: 55555}, () => {
-  // Start server
-  app.listen(PORT, () => {
-    console.log('Server listening on port ' + PORT);
-  });
+// start http server
+app.listen(HTTP_PORT, () => {
+  console.log('http server listening on port ' + HTTP_PORT);
+});
+
+//  -- GET REQUESTS --
+//#region
+
+// requests for Event streams
+app.get('/status', (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders(); // flush the headers to establish SSE with client
 });
 
 // mainpage
@@ -55,40 +79,39 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/res/index.html');
 });
 
-app.get('/app.js', (req, res) => {
-  res.sendFile(__dirname + '/res/app.js');
-});
+// app.get('/app.js', (req, res) => {
+//   res.sendFile(__dirname + '/res/app.js');
+// });
 
-app.get('/app.css', (req, res) => {
-  res.sendFile(__dirname + '/res/app.css');
-});
+// app.get('/app.css', (req, res) => {
+//   res.sendFile(__dirname + '/res/app.css');
+// });
 
 // Endpoints
 app.get('/off/', (req, res) => {
   sendMsg('colorwheel', "000000");
-  res.status(200).json({msg: 'off'});
+  res.status(200).json({"msg": 'off'});
 });
 
 app.get('/dimm/', (req, res) => {
   sendMsg('colorwheel', 'ff0032')
-  res.status(200).json({msg: 'dimm'});
+  res.status(200).json({"msg": 'dimm'});
 });
 
 app.get('/rainbow/', (req, res) => {
   sendMsg('rainbow', '0.05')
-  res.status(200).json({msg: 'rainbow'});
+  res.status(200).json({"msg": 'rainbow'});
 });
 
 app.get('/rainbow/:speed', (req, res) => {
-  let speed = req.params.speed
-  console.log(speed)
+  let speed = req.params.speed;
   sendMsg('rainbow', speed)
-  res.status(200).json({msg: 'rainbow'});
+  res.status(200).json({"msg": 'rainbow'});
 });
 
 app.get('/colorwheel/:color', (req, res) => {
   sendMsg('colorwheel', req.params.color)
-  res.status(200).json({msg: req.params.color});
+  res.status(200).json({"msg": req.params.color});
 })
 
 // ignore favicon
@@ -96,3 +119,4 @@ app.get('/favicon.ico', (req, res) => {
   res.status(204);
   res.end();
 });
+//#endregion
